133b01bfd11bc1671e193d2b1e91ef4759ddfa43
Hay-Z <hberge17@gmail.com>
2015-04-13 16:45:22 -0600

    Added old working files
    Added old working files

--- routes/courseList.js
+++ routes/courseList.js
@@ -3,20 +3,19 @@ module.exports = function(app) {
     app.get('/course/list', function(req, res) {
 
         // get the courses collection
-        var course = app.db.get('courses')
+        var courses = app.db.get('courses')
 
         // compose a query to look up docs whose 'categories' field contains the word 'Doctors'
         var q = {}
 
         // execute the query to find those matched limiting to 20
-        course.find(q, {
+        courses.find(q, {
             limit: 20
         }, function(err, docs) {
             
-            res.render('courses/list.jade', {
-                course: docs
+            res.render('course/list.jade', {
+                courses: docs
             })
-            console.log(course)
         })
     })
 
